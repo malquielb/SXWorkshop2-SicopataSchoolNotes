6e9b5a55a1f71e3f97c3db5a70663c12b49b724f
Malquiel B. H <malquielbeltreh@hotmail.com>
2022-05-10 12:49:35 -0400

    First Commit

---
+++ SicopataSchool.NotesManagement.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SicopataSchool.NotesManagement.Application.Contracts.Persistence
+{
+    internal class IAsyncRepository
+    {
+    }
+}
---
+++ SicopataSchool.NotesManagement.Domain/Entities/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SicopataSchool.NotesManagement.Domain.Entities
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+}
